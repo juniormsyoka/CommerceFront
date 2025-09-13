3a8db4dffa9c4435b2d58bd676faaed0ecc74072
John Musyoka <juniormsyoka35@gmail.com>
2025-09-13 19:29:18 +0300

    solved Fi Errors

--- src/Components/UI/EmptyState.tsx
+++ src/Components/UI/EmptyState.tsx
@@ -1,14 +1,16 @@
 import { FiEdit, FiTrash2, FiChevronLeft, FiChevronRight, FiX, FiPlus, FiEye , FiBell} from "react-icons/fi";
+import IconWrapper from "../IconWrapper";
 import { Link } from "react-router-dom";
 export const EmptyState: React.FC<{ onCreateListing: () => void }> = ({ onCreateListing }) => (
     
   <div className="empty-state">
-    <span><FiEye size={40} /></span>
+     <IconWrapper icon={FiEye} size={40} />
     <h3>No listings yet</h3>
     <p>You haven't posted any items for sale. Create your first listing to start selling.</p>
     <button onClick={onCreateListing}>
          <Link to="/postitem" className="primary-btn tech-btn">
-          <FiPlus size={18} /> Create Listing
+          <IconWrapper icon={FiPlus} size={18} />
+          Create Listing
          </Link>
      
     </button>
